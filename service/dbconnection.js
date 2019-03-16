99320cf05af615ea474d8211cf5a4ac99ae5d975
iPhra <francesco1.lorenzo@mail.polimi.it>
2019-03-16 19:43:07 +0100

    Change db settings

--- service/dbconnection.js
+++ service/dbconnection.js
@@ -5,7 +5,7 @@ let { booksDbSetup } = require("./BooksService");
 
 let sqlDb = sqlDbFactory({
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL || "postgres://bciddqzwubzgxb:c0154d9c774c7b7da88220d33d4ab6bd3be0b5b32492aacf6e62c049865f3162@ec2-54-247-70-127.eu-west-1.compute.amazonaws.com:5432/d6ifalhrov4ije?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory",
     ssl: true,
     debug: true
 });

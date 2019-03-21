9283f68ff83d7ce05f2457adbe926c7af3ccb2e7
Luca Molteni <moltek96@gmail.com>
2019-03-21 15:53:52 +0100

    Update Book POST

--- service/BooksService.js
+++ service/BooksService.js
@@ -104,58 +104,39 @@ exports.booksGET = async (offset,limit) => {
  * book BookContent The book object to insert.
  * returns inline_response_200
  **/
-exports.booksPOST = function(book) {
-  return new Promise(function(resolve, reject) {
+exports.booksPOST = async (book) => {
 
-      const data = {
-          'title': book.title,
-          'isbn10': book.isbn10,
-          'isbn13': book.isbn13,
-          'description': book.description,
-          'current_price': book.current_price,
-          'num_of_pages': book.num_of_pages,
-          'availability': book.availability,
-          'cover_type' : book.cover_type,
-          'img_path': book.imgpath
-      };
+    let data;
+    data = {
+        'title': book.title,
+        'isbn10': book.isbn10,
+        'isbn13': book.isbn13,
+        'description': book.description,
+        'current_price': book.current_price,
+        'num_of_pages': book.num_of_pages,
+        'availability': book.availability,
+        'cover_type' : book.cover_type,
+        'img_path': book.imgpath
+    };
 
-      database.transaction(trx => {
-          return trx
-              .insert(data, 'book_id')
-              .into('book')
-              .then(function(id) {
+    return database.transaction(async trx => {
+        const id = await trx.insert(data, ['book_id'])
+            .into('book');
 
-                  const data = book.genres.map(genre => {
-                      return {
-                          'book_id': id[0],
-                          'genre' : genre
-                      };
-                  });
+        data = book.genres.map(genre => {
+            return { 'book_id': id[0].book_id, 'genre' : genre };
+        });
 
-                  // Insert book_id, genre into genre table
-                  return trx.insert(data, 'genre').into('genre')
-                      .then( function(genre) {
+        await trx.insert(data, 'genre').into('genre');
 
-                          const data = book.authors.author_ids.map(author_id => {
-                              return {
-                                  'book_id': id[0],
-                                  'author_id' : author_id
-                              };
-                          });
+        data = book.authors.author_ids.map(author_id => {
+            return { 'book_id': id[0].book_id, 'author_id' : author_id };
+        });
 
-                          // Insert book_id, author_is into authorship table
-                          return trx.insert(data, 'author_id').into('authorship')
-                              .then((author_ids) => {
-                                  console.log('Author_ids', author_ids);
-                                  resolve(id[0]);
-                              })
-                              .catch(err => reject(err, 400));
-                      })
-                      .catch( err => reject(err, 400));
-              })
-              .catch( err => reject(err, 400));
-      });
-  });
+        await trx.insert(data, 'author_id').into('authorship');
+
+        return id[0];
+    });
 };
 
 

4a24de80a111f708724d84f5c5b6018a80419fb2
Luca Molteni <moltek96@gmail.com>
2019-04-19 12:31:12 +0200

    New responsive version of best sellers tab

--- public/assets/js/bestsellers.js
+++ public/assets/js/bestsellers.js
@@ -1,6 +1,17 @@
-function appendTop10() {
+async function appendTop10() {
+    console.log("Here1");
+    var books = await (await fetch(`/v2/books?limit=10`)).json();
+    console.log(books);
+    console.log("Here");
+    var author;
+    for(let i=0; i<books.length; i++) {
+        books[i]["rank"] = i+1;
+        author = await retrieveAuthor(books[i].book.author_id);
+        card_deck = i<5 ? "0-5" : "6-10";
+        fillTemplate(books[i],author, card_deck);
+    }
 
-    var promise = fetch(`/v2/books?limit=10`)
+    /*var promise = fetch(`/v2/books?limit=10`)
         .then(function(response) {
             console.log(response);
             return response.json();
@@ -12,25 +23,27 @@ function appendTop10() {
             console.log(books);
             fillTemplate(books[i]);
         }
-    }).catch( (e) => console.log(e))
+    }).catch( (e) => console.log(e))*/
 }
 
-function fillTemplate(book) {
+function fillTemplate(book, author, card_deck) {
     console.log(book);
     var book = {
         img: book.imgpath,
         title: book.book.title,
         price: book.book.current_price,
-        authors_name: "Maria",
-        authors_surname: "Callas",
-        abstract: book.bookabstract ? book.book.abstract : "Lorem ipsum dolor",
+        authors_name: author.name,
+        authors_surname: author.surname,
+        abstract: book.book.abstract ? book.book.abstract : "Lorem ipsum dolor",
         rank: book.rank
     };
     var template = $('#ListItem').html();
     var html = Mustache.to_html(template, book);
-    console.log(html);
-    console.log($('#Top10'));
-    $('#Top10').append(html);
+    $('#'+card_deck).append(html);
+}
+
+async function retrieveAuthor(author_id) {
+    return (await fetch('/v2/authors/'+author_id)).json()
 }
 
 $(function() {

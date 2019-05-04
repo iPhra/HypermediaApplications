a077def3d7dd62a3ac071b05e931508fc81dd8e1
iPhra <francesco1.lorenzo@mail.polimi.it>
2019-05-04 16:18:33 +0200

    Add add-to-cart to search results

--- public/assets/js/search-results.js
+++ public/assets/js/search-results.js
@@ -11,37 +11,35 @@ $(async function() {
 let counter = 0;
 
 function fillBook(book, author) {
-    const img = "../assets/images/"+book.book.imgpath;
+    const img = "../assets/images/" + book.book.imgpath;
     const title = book.book.title;
     const author_name = author.name;
     const author_surname = author.surname;
-    const author_link = "/pages/author.html?id="+book.book.author_id;
-    const book_link = "/pages/book.html?id="+book.book_id;
+    const author_link = "/pages/author.html?id=" + book.book.author_id;
+    const book_link = "/pages/book.html?id=" + book.book_id;
 
-    let tpl = `<div class="card">
-        <img class="card-img-top" src="`+img+`" alt="Card image cap">
+    return `<div class="card">
+        <img class="card-img-top" src="` + img + `" alt="Card image cap">
         <div class="card-body">
-            <h5 class="card-title">`+title+`</h5>
+            <h5 class="card-title">` + title + `</h5>
             <small>  Author:
-                <a href="`+author_link+`">`+author_name+` `+author_surname+`</a>
+                <a href="` + author_link + `">` + author_name + ` ` + author_surname + `</a>
             </small>
         </div>
         <div class="card-footer">
             <div class="row ">
                 <div class="col padding-10px">
-                    <a href="`+book_link+`" class="btn btn-outline-primary btn-sm">
+                    <a href="` + book_link + `" class="btn btn-outline-primary btn-sm">
                         <i class="fa fa-book"></i>
                         View Book</a>
                 </div>
                 <div class="col padding-10px">
-                    <a href="{{link_add_to_cart}}" class="btn btn-outline-primary btn-sm">
+                    <a id="` + book.book_id + `" href="#" class="btn btn-outline-primary btn-sm cart">
                         <i class="fa fa-shopping-cart"></i> Add to cart</a>
                 </div>
             </div>
         </div>
     </div>`;
-
-    return tpl;
 }
 
 async function retrieveAuthor(author_id) {
@@ -85,6 +83,34 @@ $(function() {
     $(document).on("click", "#logout", function(){
         localStorage.removeItem("token");
         location.reload();
+    });
+});
+
+$(function() {
+    $(document).on("click", ".cart", function(){
+        const token = localStorage.getItem("token");
+        if(!token) alert("You must be logged in to add items to the cart!");
+        else {
+            $.ajax({
+                url: '/v2/account/cart',
+                type: 'POST',
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader('Authorization', token);
+                },
+                dataType: "text",
+                contentType: "application/json",
+                data: JSON.stringify({
+                    book_id : parseInt($(this).attr('id'))
+                }),
+                success: function () {
+                    alert("Item added successfully!")
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    alert("Error " + jqXHR.status +
+                        ": " + errorThrown);
+                }
+            });
+        }
     });
 });
 

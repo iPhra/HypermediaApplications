497a7c3770ee97e7320536b452209954d277916f
Luca Molteni <moltek96@gmail.com>
2019-05-07 18:20:22 +0200

    Img resizing

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -29,13 +29,13 @@ function fillTop10(book, author) {
     const book_link = "/pages/book.html?id="+book.book_id;
 
     return `<a href="`+book_link+`" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
-                  <div class="flex-column">
+                  <div class="flex-column w-75">
                     `+title+`
                     <p><small>by `+author_name+` `+author_surname+`</small></p>
                     <span class="badge badge-info badge-pill"> `+genres+`</span>
                   </div>
-                  <div class="image-parent">
-                    <img src="`+img+`" class="img-fluid w-50" alt="cover">
+                  <div class="image-parent w-25">
+                    <img src="`+img+`" class="img-fluid w-100" alt="cover">
                   </div>
                 </a>`;
 }
